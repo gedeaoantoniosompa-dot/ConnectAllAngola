f5645f5126be25013f09476ef468207678be93b5
gedeaoantoniosompa-dot <gedeaoantoniosompa@gmail.com>
2026-09-02 22:48:34 +0100

    feat(admin): expose protected administrator profile operation

--- functions/adminEntry.js
+++ functions/adminEntry.js
@@ -1,9 +1,11 @@
 // Firebase Functions entrypoint.
-// Keep all existing exports and add the protected administrative gateway.
+// Keep all existing exports and add protected administrative operations.
 const existing = require('./index');
 const { adminOperation } = require('./adminGateway');
+const { updateAdminProfile } = require('./updateAdminProfile');
 
 module.exports = {
   ...existing,
   adminOperation,
+  updateAdminProfile,
 };
